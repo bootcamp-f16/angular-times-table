7ec5c465786b9cb4e7f6eb477064759894972574
Matt Smith <msmith@apaxsoftware.com>
2016-10-04 15:23:16 -0400

    Component based testing with times tables.

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
 'use strict';
 angular.module('app', [])
-    .controller('MainCtrl', mainController);
+    .component('root', {
+        template: `
+            <input type="number" ng-model="$ctrl.userRows" class="form-control" />
+            <input type="number" ng-model="$ctrl.userCols" class="form-control" />
+            <times-table rows="$ctrl.userRows" cols="$ctrl.userCols" />
+            <times-table rows="2" cols="3" />
+            <times-table rows="$ctrl.userRows" cols="$ctrl.userCols" />
+        `,
+        controller: mainController
+    })
+    .component('timesTable', {
+        templateUrl: 'times-table.html',
+        bindings: {
+            rows: '<',
+            cols: '<'
+        },
+        controller: TimesTableController
+    });
 
-function mainController($scope) {
-    $scope.userRows = 12;
-    $scope.userCols = 12;
-    $scope.selectedTableValue = null;
+function mainController() {
+    this.userRows = 6;
+    this.userCols = 6;
+}
 
-    $scope.showTable = function () {
-        return $scope.userRows > 0 && $scope.userCols > 0;
+function TimesTableController() {
+    var ctrl = this;
+
+    ctrl.selectedTableValue = null;
+
+    ctrl.showTable = function () {
+        return ctrl.rows > 0 && ctrl.cols > 0;
     };
 
-    $scope.getRows = function () {
-        return R.range(0, $scope.userRows);
+    ctrl.getRows = function () {
+        return R.range(0, ctrl.rows);
     };
 
-    $scope.getCols = function () {
-        return R.range(0, $scope.userCols);
+    ctrl.getCols = function () {
+        return R.range(0, ctrl.cols);
     };
 
-    $scope.getTableValue = function (row, col) {
+    ctrl.getTableValue = function (row, col) {
         return (row+1) * (col+1);
     };
 
-    $scope.setSelectedTableValue = function (row, col) {
-        $scope.selectedTableValue  = {
+    ctrl.setSelectedTableValue = function (row, col) {
+        ctrl.selectedTableValue  = {
             x: row + 1,
             y: col + 1,
             result: (row+1) * (col+1)
         };
     }
 
-    $scope.resetSelectedTableValue = function () {
-        $scope.selectedTableValue = null;
+    ctrl.resetSelectedTableValue = function () {
+        ctrl.selectedTableValue = null;
     }
-
 }
